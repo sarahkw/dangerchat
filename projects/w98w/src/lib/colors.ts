4c48261424e5bea41f1f6010de1f65dfce68970b
sarah <netcatkate@gmail.com>
2022-02-28 16:40:20 -0800

    try out branding

---
+++ projects/w98w/src/lib/colors.ts
@@ -0,0 +1,15 @@
+type Color = string & { __brand: "w98w-color" };
+
+export abstract class Colors {
+
+    static readonly WIDGET_TEXT = "black" as Color;
+    static readonly WIDGET_BG = "#c0c0c0" as Color;
+
+    // Bevel
+    static readonly BEV_BLACK = "#000000" as Color;
+    static readonly BEV_DARKGRAY = "#808080" as Color;
+    static readonly BEV_GRAY = Colors.WIDGET_BG; // Not used, but here to show how the bevel colors work.
+    static readonly BEV_LIGHTGRAY = "#dfdfdf" as Color;
+    static readonly BEV_WHITE = "#ffffff" as Color;
+
+}
