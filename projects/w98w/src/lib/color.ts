aa3e5da1b9895b8fc1224565728687b3160244e3
sarah <netcatkate@gmail.com>
2022-02-28 16:04:40 -0800

    put in the colors

--- projects/w98w/src/lib/color.ts
+++ projects/w98w/src/lib/color.ts
@@ -1,2 +1,11 @@
-export class Color {
+export abstract class Color {
+
+    static readonly TEXT = "black";
+
+    static readonly BEV_BLACK = "#000000";
+    static readonly BEV_DARKGRAY = "#808080";
+    static readonly BEV_GRAY = "#c0c0c0";
+    static readonly BEV_LIGHTGRAY = "#dfdfdf";
+    static readonly BEV_WHITE = "#ffffff";
+
 }
