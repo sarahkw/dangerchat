cc03969eb4f4d0bbc1911bbfe8bf3a0e9ea9f11d
sarah <netcatkate@gmail.com>
2022-02-28 16:14:15 -0800

    Add comment

--- projects/w98w/src/lib/color.ts
+++ projects/w98w/src/lib/color.ts
@@ -3,6 +3,7 @@ export abstract class Color {
     static readonly WIDGET_TEXT = "black";
     static readonly WIDGET_BG = "#c0c0c0";
 
+    // Bevel
     static readonly BEV_BLACK = "#000000";
     static readonly BEV_DARKGRAY = "#808080";
     static readonly BEV_GRAY = Color.WIDGET_BG; // Not used, but here to show how the bevel colors work.
