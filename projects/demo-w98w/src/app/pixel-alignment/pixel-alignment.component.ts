a5fca35ca98feca2c60f914b934391c0ca826b17
sarah <netcatkate@gmail.com>
2022-03-03 12:25:26 -0800

    demo to support all those directions

--- projects/demo-w98w/src/app/pixel-alignment/pixel-alignment.component.ts
+++ projects/demo-w98w/src/app/pixel-alignment/pixel-alignment.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PixelAlignmentComponent implements OnInit {
 
+  directions = ["top", "bottom", "left", "right"];
+
+  direction = this.directions[0];
   csswidth = 90;
   cssheight = 100;
   canvaswidth = 90;
@@ -29,27 +32,70 @@ export class PixelAlignmentComponent implements OnInit {
     let oCtx = oCanvas.getContext('2d')!;
 
     const colors = ['black', '#808080'];
-    let pos = this.canvasheight;
+    let pos = 0;
+
+    switch (this.direction) {
+      case "top":
+      case "left":
+        pos = 0;
+        break;
+      case "bottom":
+      case "right":
+        pos = this.canvasheight;
+        break;
+    }
+
     for (let i = 0; i < this.altcount; ++i) {
-      pos -= this.pixelsize;
       oCtx.fillStyle = colors[i % colors.length];
-      oCtx.fillRect(0, pos, oCanvas.width, this.pixelsize);
+
+      switch (this.direction) {
+        case "top":
+          oCtx.fillRect(0, pos, oCanvas.width, this.pixelsize);
+          pos += this.pixelsize;
+          break;
+        case "left":
+          oCtx.fillRect(pos, 0, this.pixelsize, oCanvas.height);
+          pos += this.pixelsize;
+          break;
+        case "bottom":
+          pos -= this.pixelsize;
+          oCtx.fillRect(0, pos, oCanvas.width, this.pixelsize);
+          break;
+        case "right":
+          pos -= this.pixelsize;
+          oCtx.fillRect(pos, 0, this.pixelsize, oCanvas.height);
+          break;
+      }
     }
 
     this.drawTargetStyle = {
       'background-image': `url(${oCanvas.toDataURL()})`,
-      'background-size': `${this.csswidth}px ${this.cssheight}px`
+      'background-size': `${this.csswidth}px ${this.cssheight}px`,
+      'background-position': `${this.direction} center`
     };
 
+  }
+
+  onAutoAssignCssWidth() {
+    this.csswidth = this.pixelsize * this.altcount;
   }
 
   onAutoAssignCssHeight() {
     this.cssheight = this.pixelsize * this.altcount;
   }
 
+  rawAutoCanvasWidthPreview() {
+    // xxx: depend on someone else to subscribe to changes
+    return this.csswidth * window.devicePixelRatio;
+  }
+
   rawAutoCanvasHeightPreview() {
     // xxx: depend on someone else to subscribe to changes
     return this.cssheight * window.devicePixelRatio;
+  }
+
+  onAutoAssignCanvasWidth() {
+    this.canvaswidth = Math.round(this.csswidth * window.devicePixelRatio);
   }
 
   onAutoAssignCanvasHeight() {
