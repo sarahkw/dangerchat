0b3f29580e35779d3fbb6e544a91ef0d5ede4b40
sarah <netcatkate@gmail.com>
2022-03-03 00:08:24 -0800

    demo-w98w: devicePixelRatio now updates

--- projects/demo-w98w/src/app/app.component.ts
+++ projects/demo-w98w/src/app/app.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, Optional } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'demo-w98w';
 
-  devicePixelRatio = window.devicePixelRatio;
+  devicePixelRatio: number | undefined;
+
+  ngOnInit(): void {
+
+    const updatePixelRatio = () => {
+      this.devicePixelRatio = window.devicePixelRatio;
+      matchMedia(`(resolution: ${this.devicePixelRatio}dppx)`).addEventListener("change", updatePixelRatio, { once: true })
+    }
+
+    updatePixelRatio();
+
+  }
 }
