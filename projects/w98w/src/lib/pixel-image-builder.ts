7f7a5fe9c86c88ee2ea17c3af1be31fa00911ccc
sarah <netcatkate@gmail.com>
2022-03-07 18:33:17 -0800

    PixelImageBuilderBasic introduced

--- projects/w98w/src/lib/pixel-image-builder.ts
+++ projects/w98w/src/lib/pixel-image-builder.ts
@@ -8,38 +8,78 @@ export type DisplayImage = {
     url: string
 };
 
-class PixelImageBuilderCol {
+export enum RowOrigin {
+    L, R
+}
+
+export enum ColOrigin {
+    T, B
+}
+
+class PixelImageBuilderBasic {
     private canvas = document.createElement('canvas');
     private context: CanvasRenderingContext2D;
 
-    private pos: number = 0;
-    private reverse: boolean = false;
-
     private pixelSize: number;
 
-    constructor(private pdc: PixelDrawConfig, artPixelHeight: number) {
+    private beginX: number;
+    private beginY: number;
+
+    constructor(private pdc: PixelDrawConfig, artPixelWidth: number, artPixelHeight: number, private hAlign: RowOrigin, private vAlign: ColOrigin) {
         this.pixelSize = pdc.pixelCanvasSize;
 
-        this.canvas.width = 1;
-        this.canvas.height = pdc.snapSize(artPixelHeight * this.pixelSize);
+        this.canvas.width = Math.max(pdc.snapSize(artPixelWidth * this.pixelSize), 1);
+        this.canvas.height = Math.max(pdc.snapSize(artPixelHeight * this.pixelSize), 1);
+
+        if (hAlign == RowOrigin.R) {
+            this.beginX = this.canvas.width - artPixelWidth * this.pixelSize;
+        } else {
+            this.beginX = 0;
+        }
+
+        if (vAlign == ColOrigin.B) {
+            this.beginY = this.canvas.height - artPixelHeight * this.pixelSize;
+        } else {
+            this.beginY = 0;
+        }
 
         this.context = this.canvas.getContext('2d')!;
     }
 
-    [reverse]() {
-        this.pos = this.canvas.height;
-        this.reverse = true;
+    drawPixel(fillStyle: string, x: number, y: number) {
+        this.context.fillStyle = fillStyle;
+        this.context.fillRect(this.beginX + x * this.pixelSize, this.beginY + y * this.pixelSize, this.pixelSize, this.pixelSize);
+    }
+
+    build(): DisplayImage {
+        return {
+            cssWidth: this.pdc.canvasSizeToCssSize(this.canvas.width),
+            cssHeight: this.pdc.canvasSizeToCssSize(this.canvas.height),
+            url: this.canvas.toDataURL()
+        };
+    }
+}
+
+class PixelImageBuilderCol extends PixelImageBuilderBasic {
+
+    private pos: number = 0;
+    private reverse: boolean = false;
+
+    constructor(pdc: PixelDrawConfig, private artPixelHeight: number, origin: ColOrigin) {
+        super(pdc, 0, artPixelHeight, RowOrigin.L, origin);
+        if (origin == ColOrigin.B) {
+            this.pos = this.artPixelHeight;
+            this.reverse = true;
+        }
     }
 
     pushPixel(fillStyle: string) {
-        this.context.fillStyle = fillStyle;
-
         if (this.reverse) {
-            this.pos -= this.pixelSize;
-            this.context.fillRect(0, this.pos, 1, this.pixelSize);
+            this.pos--;
+            this.drawPixel(fillStyle, 0, this.pos);
         } else {
-            this.context.fillRect(0, this.pos, 1, this.pixelSize);
-            this.pos += this.pixelSize;
+            this.drawPixel(fillStyle, 0, this.pos);
+            this.pos++;
         }
         return this;
     }
@@ -49,14 +89,6 @@ class PixelImageBuilderCol {
             this.pushPixel(fillStyle);
         }
         return this;
-    }
-
-    build(): DisplayImage {
-        return {
-            cssWidth: this.pdc.canvasSizeToCssSize(this.canvas.width),
-            cssHeight: this.pdc.canvasSizeToCssSize(this.canvas.height),
-            url: this.canvas.toDataURL()
-        };
     }
 }
 
@@ -112,14 +144,6 @@ class PixelImageBuilderRow {
     }
 }
 
-export enum RowOrigin {
-    L, R
-}
-
-export enum ColOrigin {
-    T, B
-}
-
 export enum GridOrigin {
     TL, TR, BL, BR
 }
@@ -128,11 +152,7 @@ export class PixelImageBuilderFactory {
     constructor(private pdc: PixelDrawConfig) {}
 
     col(origin: ColOrigin, artPixelHeight: number) {
-        const ret = new PixelImageBuilderCol(this.pdc, artPixelHeight);
-        if (origin === ColOrigin.B) {
-            ret[reverse]();
-        }
-        return ret;        
+        return new PixelImageBuilderCol(this.pdc, artPixelHeight, origin);
     }
 
     row(origin: RowOrigin, artPixelWidth: number) {
