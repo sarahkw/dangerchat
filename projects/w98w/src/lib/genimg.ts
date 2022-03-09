206eaa2fcef6af28a9d33d1efb4f8a8becdf114a
sarah <netcatkate@gmail.com>
2022-03-09 15:25:38 -0800

    improve genImg API

--- projects/w98w/src/lib/genimg.ts
+++ projects/w98w/src/lib/genimg.ts
@@ -1,9 +1,15 @@
 import { DisplayImage, PixelImageBuilderFactory } from "./pixel-image-builder";
 
 export type GenImgDescriptor = {
-    w: number,
-    h: number,
-    draw: (w: number, h: number, pibf: PixelImageBuilderFactory) => DisplayImage;
+
+    // the descriptor hints what size they should be in CSS pixels. can be undefined if
+    // the image will just draw for any provided size.
+    desiredCssWidth: number,
+    desiredCssHeight: number,
+
+    // the width and height will be the desired ones if those are available. otherwise,
+    // it'll be what's requested.
+    draw: (drawCssWidth: number, drawCssHeight: number, pibf: PixelImageBuilderFactory) => DisplayImage;
 };
 
 export abstract class GenImg {
