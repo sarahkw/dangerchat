313e1bde512a2b44d6bb96d7a292a7dd6daf40a2
sarah <netcatkate@gmail.com>
2022-03-10 15:16:24 -0800

    sizing titlebar height based on font px

--- projects/w98w/src/lib/w98w-styles.ts
+++ projects/w98w/src/lib/w98w-styles.ts
@@ -1,9 +1,48 @@
 
+function fontSzToTitleBarSz(fontSz: number) {
+    // by observation on Display Properties -> Appearance -> Active Title Bar
+    const LOOKUP = [
+        [1 , 18 ],
+        [12, 22 ],
+        [14, 26 ],
+        [17, 28 ],
+        [18, 31 ],
+        [20, 34 ],
+        [23, 39 ],
+        [27, 50 ],
+        [33, 54 ],
+        [36, 60 ],
+        [39, 66 ],
+        [41, 67 ],
+        [47, 76 ],
+        [48, 82 ],
+        [53, 89 ],
+        [59, 100],
+    ];
+
+    let val!: number;
+
+    for (let [k, v] of LOOKUP) {
+        if (fontSz >= k) {
+            val = v;
+        } else {
+            break;
+        }
+    }
+
+    console.assert(val !== undefined);
+
+    return val;
+}
+
+const titleBarFont = 16;
+
 export abstract class W98wStyles {
 
     // also used for: taskbar button size, ...
     static readonly titleBar = {
-        'font-size.px': 16,
+        font: titleBarFont,
+        barHeight: fontSzToTitleBarSz(titleBarFont)
     };
 
 };
