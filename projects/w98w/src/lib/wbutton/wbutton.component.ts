e0a54906fc19463bb1098b042cedf817715e618d
sarah <netcatkate@gmail.com>
2022-03-12 20:27:52 -0800

    Copy over the bevels from Button to WButton

--- projects/w98w/src/lib/wbutton/wbutton.component.ts
+++ projects/w98w/src/lib/wbutton/wbutton.component.ts
@@ -1,5 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Bevels } from '../bevel';
+import { GenCssInput, genGenCssInput, Bevel8SplitComponent } from '../bevel-8split/bevel-8split.component';
 import { Colors } from '../colors';
+import { PixelImageBuilderFactory } from '../pixel-image-builder';
+import { PixelImageDrawer } from '../pixel-image-drawer';
+import { PixelImageService } from '../pixel-image.service';
+import { StyleInjector } from '../style-injector';
 import { W98wStyles } from '../w98w-styles';
 
 @Component({
@@ -7,7 +13,7 @@ import { W98wStyles } from '../w98w-styles';
   templateUrl: './wbutton.component.html',
   styleUrls: ['./wbutton.component.css']
 })
-export class WButtonComponent implements OnInit {
+export class WButtonComponent implements OnInit, OnDestroy {
 
   @Input() width: number | undefined;
   @Input() height: number | undefined;
@@ -16,9 +22,52 @@ export class WButtonComponent implements OnInit {
   readonly STYLES = W98wStyles;
   readonly COLORS = Colors;
 
-  constructor() { }
+  constructor(private imgService: PixelImageService) {}
 
   ngOnInit(): void {
+    this.imgService.pidRegister(WButtonComponent.PID_NORMAL);
+    this.imgService.pidRegister(WButtonComponent.PID_PRESSED);
   }
+
+  ngOnDestroy(): void {
+    this.imgService.pidUnregister(WButtonComponent.PID_PRESSED);
+    this.imgService.pidUnregister(WButtonComponent.PID_NORMAL);
+  }
+
+  //#region PIDs
+
+  static readonly PID_NORMAL = new class implements PixelImageDrawer {
+    private styleInjector = new StyleInjector();
+
+    pidGenerateImages(pibf: PixelImageBuilderFactory): GenCssInput {
+      return genGenCssInput(ri => Bevels.BUTTON.genImage(ri, pibf));
+    }
+
+    pidApplyImages(imgs: GenCssInput): void {
+      this.styleInjector.replaceStyle(Bevel8SplitComponent.genCss(".w98w-wbutton", imgs));
+    }
+
+    pidDestroy(): void {
+      this.styleInjector.destroy();
+    }
+  };  // PID_NORMAL
+
+  static readonly PID_PRESSED = new class implements PixelImageDrawer {
+    private styleInjector = new StyleInjector();
+
+    pidGenerateImages(pibf: PixelImageBuilderFactory): GenCssInput {
+      return genGenCssInput(ri => Bevels.BUTTON_PRESSED.genImage(ri, pibf));
+    }
+
+    pidApplyImages(imgs: GenCssInput): void {
+      this.styleInjector.replaceStyle(Bevel8SplitComponent.genCss(".w98w-wbutton:active", imgs));
+    }
+
+    pidDestroy(): void {
+      this.styleInjector.destroy();
+    }
+  };  // PID_PRESSED
+
+  //#endregion
 
 }
