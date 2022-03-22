a23883e5043c2d7910ce2fba02f3c99830f43cfa
sarah <netcatkate@gmail.com>
2022-03-22 15:33:44 -0700

    menu: bevels

--- projects/w98w/src/lib/menu/menu.component.ts
+++ projects/w98w/src/lib/menu/menu.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, OnDestroy, OnInit } from '@angular/core';
+import { Bevels } from '../bevel';
+import { Bevel8SplitComponent, GenCssInput, genGenCssInput } from '../bevel-8split/bevel-8split.component';
+import { Colors } from '../colors';
+import { PixelImageBuilderFactory } from '../pixel-image-builder';
+import { PixelImageDrawer } from '../pixel-image-drawer';
+import { PixelImageService } from '../pixel-image.service';
+import { StyleInjector } from '../style-injector';
 
 @Component({
   selector: 'menu[w98w-menu]',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  @HostBinding('class') hbClass = 'w98w-menu';
+  @HostBinding('style.backgroundColor') hbSBC = Colors.WIDGET_BG;
+  @HostBinding('style.padding') hbP = `${Bevels.MENU.getPadding()}px`
+
+  constructor(private imgService: PixelImageService) { }
 
   ngOnInit(): void {
+    this.imgService.pidRegister(MenuComponent.PID);
   }
 
+  ngOnDestroy(): void {
+    this.imgService.pidUnregister(MenuComponent.PID);
+  }
+
+  static readonly PID = new class implements PixelImageDrawer {
+    private styleInjector = new StyleInjector();
+
+    pidGenerateImages(pibf: PixelImageBuilderFactory): GenCssInput {
+      return genGenCssInput(ri => Bevels.MENU.genImage(ri, pibf));
+    }
+
+    pidApplyImages(imgs: GenCssInput): void {
+      this.styleInjector.replaceStyle(Bevel8SplitComponent.genCss(".w98w-menu", imgs));
+    }
+
+    pidDestroy(): void {
+      this.styleInjector.destroy();
+    }
+  };  // PID
 }
