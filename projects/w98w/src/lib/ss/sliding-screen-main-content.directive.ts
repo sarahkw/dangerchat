5ea6a0b89fd5ccf9d0fed9e93bc72bce3211b93f
sarah <netcatkate@gmail.com>
2022-03-25 18:45:45 -0700

    modify to let ss user give main content div

--- projects/w98w/src/lib/ss/sliding-screen-main-content.directive.ts
+++ projects/w98w/src/lib/ss/sliding-screen-main-content.directive.ts
@@ -1,10 +1,20 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding } from '@angular/core';
+import { SlidingScreenComponent } from './sliding-screen/sliding-screen.component';
 
 @Directive({
   selector: '[w98w-sliding-screen-main-content]'
 })
 export class SlidingScreenMainContentDirective {
 
-  constructor() { }
+  @HostBinding('style.height') hbSH = "100%";
+  @HostBinding('style.width') get hbSW() {
+    if (this.ss.overlay) {
+      return `${this.ss.rootDiv.nativeElement.clientWidth}px`;
+    } else {
+      return "100%";
+    }
+  }
+
+  constructor(private ss: SlidingScreenComponent) { }
 
 }
