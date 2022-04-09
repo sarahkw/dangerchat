239f8b291d7543f13a076b90ef7cf68c979f5697
sarah <netcatkate@gmail.com>
2022-04-08 18:44:43 -0700

    overflow test, also showing a found bug

--- projects/demo-w98w/src/app/test-menu-lib/test-menu-lib.component.ts
+++ projects/demo-w98w/src/app/test-menu-lib/test-menu-lib.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { MenuTemplateDirective } from 'projects/w98w/src/lib/menu/menu-template.directive';
+import { asapScheduler, Observable, observeOn, ReplaySubject } from 'rxjs';
 
 @Component({
   selector: 'app-test-menu-lib',
   templateUrl: './test-menu-lib.component.html',
   styleUrls: ['./test-menu-lib.component.css']
 })
-export class TestMenuLibComponent implements OnInit {
+export class TestMenuLibComponent implements OnInit, AfterViewInit {
 
   @ViewChild('menuTemp') menuTemp!: MenuTemplateDirective;
 
@@ -15,7 +16,16 @@ export class TestMenuLibComponent implements OnInit {
     return this.menuTemp;
   }).bind(this);
 
+  // TODO this is a lot of boilerplate maybe we need to factor it
+  @ViewChild('overflowWndTemp') overflowWndTemp!: TemplateRef<any>;
+  private overflowWndTemp$: ReplaySubject<typeof this.overflowWndTemp> = new ReplaySubject(1);
+  overflowWndTempDeferred$ = this.overflowWndTemp$.pipe(observeOn(asapScheduler));
+
   constructor() { }
+
+  ngAfterViewInit(): void {
+    this.overflowWndTemp$.next(this.overflowWndTemp);
+  }
 
   ngOnInit(): void {
   }
