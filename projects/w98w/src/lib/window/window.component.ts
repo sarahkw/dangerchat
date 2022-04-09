5d535c69cd360232a2a3c55b72ee1c980be0e490
sarah <netcatkate@gmail.com>
2022-04-08 17:18:02 -0700

    windows now can have frames

--- projects/w98w/src/lib/window/window.component.ts
+++ projects/w98w/src/lib/window/window.component.ts
@@ -1,4 +1,10 @@
-import { Component, Directive, HostBinding, Input, OnInit } from '@angular/core';
+import { Component, Directive, HostBinding, Input, OnDestroy, OnInit } from '@angular/core';
+import { Bevels } from '../bevel';
+import { GenCssInput, genGenCssInput, Bevel8SplitComponent } from '../bevel-8split/bevel-8split.component';
+import { PixelImageBuilderFactory } from '../pixel-image-builder';
+import { PixelImageDrawer } from '../pixel-image-drawer';
+import { PixelImageService } from '../pixel-image.service';
+import { StyleInjector } from '../style-injector';
 
 @Directive({ selector: '[w98w-window-title-bar]'})
 export class WindowTitleBarDirective {
@@ -15,13 +21,42 @@ export class WindowMenuBarDirective {
   templateUrl: './window.component.html',
   styleUrls: ['./window.component.scss']
 })
-export class WindowComponent implements OnInit {
+export class WindowComponent implements OnInit, OnDestroy {
 
   @Input() drawFrame = true;  // if maximized this will be false
 
-  constructor() { }
+  @HostBinding('class') hbc = 'w98w-window';
+
+  @HostBinding('style.padding.px') get hbsPadding() {
+    // 2 extra pixels spacing, as seen in screenshot
+    return this.drawFrame && (Bevels.WINDOW.getPadding() + 2);
+  }
+
+  constructor(private imgService: PixelImageService) { }
 
   ngOnInit(): void {
+    this.imgService.pidRegister(WindowComponent.PID_FRAME);
   }
+
+  ngOnDestroy(): void {
+    this.imgService.pidUnregister(WindowComponent.PID_FRAME);
+  }
+
+  static readonly PID_FRAME = new class implements PixelImageDrawer<GenCssInput> {
+    private styleInjector = new StyleInjector();
+
+    pidGenerateImages(pibf: PixelImageBuilderFactory): GenCssInput {
+      return genGenCssInput(ri => Bevels.WINDOW.genImage(ri, pibf));
+    }
+
+    pidApplyImages(imgs: GenCssInput): void {
+      this.styleInjector.replaceStyle(Bevel8SplitComponent.genCss(".w98w-window", imgs));
+    }
+
+    pidDestroy(): void {
+      this.styleInjector.destroy();
+    }
+  };  // PID_FRAME
+
 
 }
