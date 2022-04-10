6fd9ddac1a6d423098043343cb605e65ccade3c3
sarah <netcatkate@gmail.com>
2022-04-09 20:15:06 -0700

    factor out templateViewChildAsap

--- projects/demo-w98w/src/app/test-shared-lib/test-shared-lib.component.ts
+++ projects/demo-w98w/src/app/test-shared-lib/test-shared-lib.component.ts
@@ -1,31 +1,32 @@
-import { AfterViewInit, Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { MenuTemplateDirective } from 'projects/w98w/src/lib/menu/menu-template.directive';
-import { asapScheduler, observeOn, ReplaySubject } from 'rxjs';
+import { templateViewChildAsap } from 'projects/w98w/src/lib/rx/template-view-child-asap';
 
 @Component({
   selector: 'app-test-shared-lib',
   templateUrl: './test-shared-lib.component.html',
   styleUrls: ['./test-shared-lib.component.scss']
 })
-export class TestSharedLibComponent implements OnInit, AfterViewInit {
+export class TestSharedLibComponent implements OnInit, AfterViewInit, OnDestroy {
 
   @ViewChild('menuTemp') menuTemp!: MenuTemplateDirective;
 
   // workaround to be able to use the value as an input param, as views are resolved after inputs are
   readonly menuTempDeferred = () => this.menuTemp;
 
-  // TODO this is a lot of boilerplate maybe we need to factor it
-  @ViewChild('overflowWndTemp') overflowWndTemp!: TemplateRef<any>;
-  private overflowWndTemp$: ReplaySubject<typeof this.overflowWndTemp> = new ReplaySubject(1);
-  overflowWndTempDeferred$ = this.overflowWndTemp$.pipe(observeOn(asapScheduler));
+  @ViewChild('overflowWndTemp') private overflowWndTemp!: TemplateRef<any>;
+  readonly overflowWndTempAsap = templateViewChildAsap(() => this.overflowWndTemp);
 
   constructor() { }
 
   ngAfterViewInit(): void {
-    this.overflowWndTemp$.next(this.overflowWndTemp);
+    this.overflowWndTempAsap.check();
   }
 
   ngOnInit(): void {
   }
 
+  ngOnDestroy(): void {
+    this.overflowWndTempAsap.complete();
+  }
 }
---
+++ projects/w98w/src/lib/rx/template-view-child-asap.ts
@@ -0,0 +1,42 @@
+import { EventEmitter } from "@angular/core";
+import { Observable, observeOn, asapScheduler } from "rxjs";
+
+// This solves the issue where you have a component which is a library of templates that you
+// want to use with ngTemplateOutlet. The problem is that ngTemplateOutlet takes the template from
+// an input variable which gets resolved before the view children are set.
+//
+// asapScheduler seems to let us render the value without causing some rendered frame where
+// the template didn't try to get rendered.
+
+export function templateViewChildAsap<T>(getter: () => T) {
+    let checkEvent = new EventEmitter<null>();
+    let completeEvent = new EventEmitter<null>();
+
+    return {
+        $: new Observable<T>(subscriber => {
+            let hasSent = false;
+            let last: T;
+
+            function check_fn() {
+                const candidate = getter();
+                if (!hasSent) {
+                    hasSent = true;
+                    last = candidate;
+                    subscriber.next(candidate);
+                } else if (candidate !== last) {
+                    last = candidate;
+                    subscriber.next(candidate);
+                }
+            }
+
+            const subscription = checkEvent.subscribe(() => check_fn());
+            subscription.add(completeEvent.subscribe(() => subscriber.complete()));
+
+            return subscription;
+
+        }).pipe(observeOn(asapScheduler)),
+
+        check: () => checkEvent.next(null),
+        complete: () => completeEvent.next(null)
+    };
+}
