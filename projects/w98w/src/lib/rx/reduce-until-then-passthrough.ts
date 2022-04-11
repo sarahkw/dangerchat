d1c4128d08d2597cffff111539bead018ea8101c
sarah <netcatkate@gmail.com>
2022-04-11 00:03:22 -0700

    lint: reduce-until-then-passthrough

--- projects/w98w/src/lib/rx/reduce-until-then-passthrough.ts
+++ projects/w98w/src/lib/rx/reduce-until-then-passthrough.ts
@@ -2,15 +2,15 @@ import { Observable, Observer } from "rxjs";
 
 export function reduceUntilThenPassthrough<T>(
     // reduce
-    accumulator: (acc: T, value: T) => T,
-    seed: any | undefined,
+    accumulator: (acc: T | undefined, value: T) => T,
+    seed: T | undefined,
 
     // until - runs on the output of the accumulator
     predicate: (value: T | undefined) => boolean
 ) {
     return function (source: Observable<T>) {
         return new Observable<T>(subscriber => {
-            let state: any = seed;
+            let state: T | undefined = seed;
             let passthrough = predicate(state);
 
             return source.subscribe(new class implements Observer<T> {
