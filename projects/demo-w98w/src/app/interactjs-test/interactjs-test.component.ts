907fa441c71e339027ee33d826e5b47a544a2924
sarah <netcatkate@gmail.com>
2022-04-11 21:33:37 -0700

    begin using interactjs

--- projects/demo-w98w/src/app/interactjs-test/interactjs-test.component.ts
+++ projects/demo-w98w/src/app/interactjs-test/interactjs-test.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+import interact from 'interactjs';
 
 @Component({
   selector: 'app-interactjs-test',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InteractjsTestComponent implements OnInit {
 
+  @ViewChild('resizable', { static: true }) resizableElement!: ElementRef<HTMLElement>;
+  @ViewChild('draggable', { static: true }) draggableElement!: ElementRef<HTMLElement>;
+
   constructor() { }
 
   ngOnInit(): void {
+    interact(this.resizableElement.nativeElement)
+      .resizable({
+        edges: { top: true, right: true, bottom: true, left: true },
+        listeners: {
+          move(event) {
+            console.log(event);
+          }
+        }
+      });
+
+    interact(this.draggableElement.nativeElement)
+      .draggable({
+        listeners: {
+          start(event) {
+            console.log('start', event);
+          },
+          move(event) {
+            console.log('move', event);
+          }
+        }
+      });
   }
 
 }
