0f6ec117d51793cab79daec5e755926143d13da1
sarah <netcatkate@gmail.com>
2022-07-01 11:01:28 -0700

    cleanup

--- projects/demo-w98w/src/app/interactjs-test/interactjs-test.component.ts
+++ projects/demo-w98w/src/app/interactjs-test/interactjs-test.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
 
-import { MoveResizeMode } from 'projects/w98w/src/lib/window/window.component';
-
 @Component({
   selector: 'app-interactjs-test',
   templateUrl: './interactjs-test.component.html',
   styleUrls: ['./interactjs-test.component.scss']
 })
 export class InteractjsTestComponent {
-  readonly MOVERESIZEMODE = MoveResizeMode;
 }
