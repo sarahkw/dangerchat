5b17273655610ae41be195ca60914de55f4301c4
sarah <netcatkate@gmail.com>
2022-09-04 14:52:41 -0700

    textarea to have depth

--- projects/w98w/src/lib/textarea/textarea.component.ts
+++ projects/w98w/src/lib/textarea/textarea.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Bevels } from '../bevel';
+import { GenCssInput, genGenCssInput, Bevel8SplitComponent } from '../bevel-8split/bevel-8split.component';
+import { PixelImageBuilderFactory } from '../pixel-image-builder';
+import { PixelImageDrawer } from '../pixel-image-drawer';
+import { PixelImageService } from '../pixel-image.service';
+import { StyleInjector } from '../style-injector';
 
 @Component({
   selector: 'w98w-textarea',
   templateUrl: './textarea.component.html',
   styleUrls: ['./textarea.component.scss']
 })
-export class TextareaComponent implements OnInit {
+export class TextareaComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(private imgService: PixelImageService) {}
 
   ngOnInit(): void {
+    this.imgService.pidRegister(TextareaComponent.PID_NORMAL);
   }
+
+  ngOnDestroy(): void {
+    this.imgService.pidUnregister(TextareaComponent.PID_NORMAL);
+  }
+
+  static readonly PID_NORMAL = new class implements PixelImageDrawer<GenCssInput> {
+    private styleInjector = new StyleInjector();
+
+    pidGenerateImages(pibf: PixelImageBuilderFactory): GenCssInput {
+      return genGenCssInput(ri => Bevels.INPUTBOX.genImage(ri, pibf));
+    }
+
+    pidApplyImages(imgs: GenCssInput): void {
+      this.styleInjector.replaceStyle(Bevel8SplitComponent.genCss("w98w-textarea", imgs));
+    }
+
+    pidDestroy(): void {
+      this.styleInjector.destroy();
+    }
+  };  // PID_NORMAL
 
 }
