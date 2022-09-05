4fa7f40ef1573adf84a9c399a112ee98632b4101
sarah <netcatkate@gmail.com>
2022-09-05 15:21:57 -0700

    new component for notepad publish

---
+++ projects/demo-w98w/src/app/program/notepad/notepad-publish/notepad-publish.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-program-notepad-publish',
+  templateUrl: './notepad-publish.component.html',
+  styleUrls: ['./notepad-publish.component.scss']
+})
+export class ProgramNotepadPublishComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
