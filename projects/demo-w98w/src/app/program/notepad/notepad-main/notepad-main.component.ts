4e434c8a5f2c6d919ab6db0c3193edc84227cfdc
sarah <netcatkate@gmail.com>
2022-09-05 15:34:00 -0700

    publish dialog

--- projects/demo-w98w/src/app/program/notepad/notepad-main/notepad-main.component.ts
+++ projects/demo-w98w/src/app/program/notepad/notepad-main/notepad-main.component.ts
@@ -1,25 +1,43 @@
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { TextareaComponent } from 'projects/w98w/src/lib/textarea/textarea.component';
 import { WindowCloserContext } from 'projects/w98w/src/lib/window/window.component';
+import { Subscription } from 'rxjs';
+import { ProgramNotepadPublishComponent } from '../notepad-publish/notepad-publish.component';
 
 @Component({
   selector: 'app-program-notepad-main',
   templateUrl: './notepad-main.component.html',
   styleUrls: ['./notepad-main.component.scss']
 })
-export class ProgramNotepadMainComponent implements OnInit {
+export class ProgramNotepadMainComponent implements OnInit, OnDestroy {
 
   @ViewChild(TextareaComponent, { static: true }) textareaComponent!: TextareaComponent;
 
   @Input() windowCloser?: WindowCloserContext;
 
+  private publishDialogSubscription?: Subscription;
+
   constructor() { }
 
-
   ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+    this.publishDialogSubscription?.unsubscribe();
+    this.publishDialogSubscription = undefined;
   }
 
   new_() {
     this.textareaComponent.childTextArea.nativeElement.value = '';
   }
+
+  publish() {
+    this.publishDialogSubscription?.unsubscribe();
+    this.publishDialogSubscription = undefined;
+
+    if (this.windowCloser) {
+      this.publishDialogSubscription = this.windowCloser.launchGenericSubscription(ProgramNotepadPublishComponent).subscribe();
+    }
+  }
+
 }
--- projects/demo-w98w/src/app/program/notepad/notepad-publish/notepad-publish.component.ts
+++ projects/demo-w98w/src/app/program/notepad/notepad-publish/notepad-publish.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { WindowCloserContext } from 'projects/w98w/src/lib/window/window.component';
 
 @Component({
   selector: 'app-program-notepad-publish',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notepad-publish.component.scss']
 })
 export class ProgramNotepadPublishComponent implements OnInit {
+
+  @Input() windowCloser?: WindowCloserContext;
 
   constructor() { }
 
