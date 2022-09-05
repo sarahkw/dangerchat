8e9195e14a00754ce50ff53fcab2f655e47e03a6
sarah <netcatkate@gmail.com>
2022-09-04 22:26:06 -0700

    Revert "Fix 100vh problem for iPad, where 100vh includes the tab bar"
    This reverts commit e0d1de43900c5daf1befdd46a6be506a70fecc79.
    
    Has problems.

--- projects/demo-w98w/src/app/desktop-test/desktop-test.component.ts
+++ projects/demo-w98w/src/app/desktop-test/desktop-test.component.ts
@@ -1,24 +1,15 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { StyleInjector } from 'projects/w98w/src/lib/style-injector';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-desktop-test',
   templateUrl: './desktop-test.component.html',
   styleUrls: ['./desktop-test.component.scss']
 })
-export class DesktopTestComponent implements OnInit, OnDestroy {
-
-  private styleInjector = new StyleInjector();
+export class DesktopTestComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
-    // Mobile browsers to not have 100vh include various bars.
-    // As a side effect, can't fling the contents around even if no overflow.
-    this.styleInjector.replaceStyle("body { overflow: hidden; }");
   }
 
-  ngOnDestroy(): void {
-    this.styleInjector.destroy();
-  }
 }
